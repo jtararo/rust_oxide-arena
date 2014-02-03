df0e5eda4bad008590e83eb0a8bce03e833c23f4
jtararo <jtararo@gmail.com>
2014-02-03 22:25:54 +1300

    Working with Arrays
    I want to store coords in a table, then be able to pick a random coords
    from the table

--- testPlugin.lua
+++ testPlugin.lua
@@ -3,11 +3,14 @@ PLUGIN.Description = "Example Plugin"
 PLUGIN.Author = "Your Name"
 PLUGIN.Version = "1.0"
 
+print(PLUGIN.Title .. " plugin loaded")
+
 -- Called when oxide loads or user types oxide.reload example at F1 console
 function PLUGIN:Init()
 	self.arenaArmed = "no";
     self:AddChatCommand("status", self.cmdStatus);
 	self:AddChatCommand("status1", self.cmdStatus1);
+	
 	
 end
 
@@ -26,27 +29,46 @@ if ( user == "3lindNinja" ) then
 	end
 end
 
--- Arm the Arena
---[[ function PLUGIN:cmdStatus1( netuser, cmd, args )
-rust.SendChatToUser( netuser, "/status1 Command Called" )
-	if self.arenaArmed == "no" then
-		rust.SendChatToUser( netuser, "Arena is Not Armed!" )
-	else
-		rust.SendChatToUser( netuser, "Arena is Armed" )
-	end
-end ]]
 function PLUGIN:cmdStatus1( netuser, cmd, args )
+
 user = netuser.DisplayName
 	if ( user == "3lindNinja" ) then
+		rust.SendChatToUser( netuser, "/status1 Command Called" )
 		if ( args[1] == nil ) then
-		rust.SendChatToUser( netuser, "/status1 Command Called. No Arguments." )
+		rust.SendChatToUser( netuser, "No Arguments." )
 			if self.arenaArmed == "no" then
 				rust.SendChatToUser( netuser, "Arena is Not Armed!" )
 			else
 				rust.SendChatToUser( netuser, "Arena is Armed" )
 			end
 		else
-			rust.SendChatToUser ( netuser, "Argument Found: " .. args[1] )
+			arg = args[1]
+			rust.SendChatToUser ( netuser, "Argument Found: " .. arg )
+			
+			-- Some Random Data
+			data = {}
+			data.1.coords.x = 100
+			data.1.coords.y = 110
+			data.1.coords.z = 120
+
+			data.2.coords.x = 200
+			data.2.coords.y = 210
+			data.2.coords.z = 220
+
+			data.3.coords.x = 300
+			data.3.coords.y = 310
+			data.3.coords.z = 320
+
+			data.4.coords.x = 400
+			data.4.coords.y = 410
+			data.4.coords.z = 420
+			
+			if ( data.arg != nil ) then
+				rust.SendChatToUser( netuser, "Coords: x=" .. data.arg.coords.x .. " y=" .. data.arg.coords.y .. " y=" .. data.arg.coords.z )
+				else
+				rust.SendChatTouser( netuser, "Invalid choice!" )
+				end
+			
 		end
 			
 			
@@ -55,6 +77,26 @@ user = netuser.DisplayName
 	end
 end
 
+-- Define some random data
+function PLUGIN:data()
+data == {}
+data.1.coords.x = 100
+data.1.coords.y = 110
+data.1.coords.z = 120
+
+data.2.coords.x = 200
+data.2.coords.y = 210
+data.2.coords.z = 220
+
+data.3.coords.x = 300
+data.3.coords.y = 310
+data.3.coords.z = 320
+
+data.4.coords.x = 400
+data.4.coords.y = 410
+data.4.coords.z = 420
+return
+end
 
 -- Automated Oxide help function (added to /help list)
 function PLUGIN:SendHelpText( netuser )
